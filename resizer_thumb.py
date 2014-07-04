ca8355a22e25ccb6c1789489ecd9b8c6fe6df99b
Jake Jung <magoja@gmail.com>
2014-07-03 23:52:51 -0700

    Thumb resizer

---
+++ resizer_thumb.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+MAXFILESIZE_BIG = 10 * 1024 # 10KB
+MAXFILESIZE_SMALL = 8 * 1024 # 10KB
+
+def IterateAndResize(inputFolder):
+  spaceSaved = 0
+  total = len(os.listdir("%s/" % inputFolder))
+  count = 0
+  print "Total %d files..." % total
+  print "-----------------"
+
+  for fn in os.listdir("%s/" % inputFolder):
+    count = count + 1
+    fullpath = "%s/%s" % (inputFolder, fn)
+    if os.path.isfile(fullpath):
+      if fn.find(".") == -1:
+        size = os.path.getsize(fullpath)
+        if fn.endswith("_s"):
+          pass
+        else:        
+          if size > MAXFILESIZE_BIG:
+            converted = ConvertThumb(inputFolder, fn)
+            spaceSaved = spaceSaved + converted
+          else:
+            print "Skip '%s/%s' (%.0fKB)" % (inputFolder, fn, size / 1024)
+
+    if count % 10 == 0:
+      print "(%d/%d)" % (count, total)
+  return spaceSaved
+
+def ConvertThumb(inputFolder, fn):
+  fullpath = "%s/%s" % (inputFolder, fn)
+  sizeBefore = os.path.getsize(fullpath)
+  os.system("mogrify -quality 80%% %s" % (fullpath))
+  sizeAfter = os.path.getsize(fullpath)
+
+  print "Convert '%s' File completed. (%.0fKB -> %.0fKB)" % (fullpath, sizeBefore / 1024, sizeAfter / 1024)
+  return sizeBefore - sizeAfter
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print "Usage:"
+    print "python resizer_thumb.py (foldername)"
+  else:
+    print "Thumb image resizer. v0.1"
+    print "  by Magoja 20140703"
+    print "-----------------------"
+
+    inputFolder = sys.argv[1]
+    if inputFolder.endswith("/"):
+      inputFolder = inputFolder[:-1]
+
+    saved = IterateAndResize(inputFolder)
+
+    print "Total %.1fMB saved" % (saved / 1024 / 1024)
